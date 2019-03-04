b86eb0c1446a97279b7a6773d8f311ffd84af75b
mike fettis <mfettis@cars.com>
2019-03-04 09:32:10 -0600

    ex

--- io-lcd.py
+++ io-lcd.py
@@ -45,13 +45,20 @@ DATA_MESSAGES = {}
 while True:
     message1 = aio.receive('pi-lcd.message1')
     message2 = aio.receive('pi-lcd.message2')
+    message3 = aio.receive('pi-lcd.message3')
+    message4 = aio.receive('pi-lcd.message4')
+    message5 = aio.receive('pi-lcd.message5')
 
     DATA_MESSAGES.update({message1: message1})
     DATA_MESSAGES.update({message2: message2})
+    DATA_MESSAGES.update({message3: message3})
+    DATA_MESSAGES.update({message4: message4})
+    DATA_MESSAGES.update({message5: message5})
 
     for data in DATA_MESSAGES:
         print(data.value)
         print_message = data.value[:16]+"\n"+data.value[16:]
+        lcd.clear()
         lcd.message = print_message
         time.sleep( 5 )
 
